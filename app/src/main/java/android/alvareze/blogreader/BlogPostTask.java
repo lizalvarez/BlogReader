2abbdda4dc0c80b6d9e2031a6e33d40aed053633
lizalvarez <li.alvarez760@gamil.com>
2014-10-10 13:59:13 -0700

    c:

--- app/src/main/java/android/alvareze/blogreader/BlogPostTask.java
+++ app/src/main/java/android/alvareze/blogreader/BlogPostTask.java
@@ -1,4 +1,37 @@
 package android.alvareze.blogreader;
 
-public class BlogPostTask {
-}
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.lang.reflect.MalformedParameterizedTypeException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject> {
+    @Override
+    protected JSONObject doInBackground(Activity... activities) {
+        try {
+            URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=10");
+
+            HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+            connection.connect();
+            int responseCode = connection.getResponseCode();
+
+            if(responseCode == HttpURLConnection.HTTP_OK){
+                 Log.i("BlogPostTask", "Successful Connection " + responseCode);
+            }
+
+        }
+        catch(MalformedParameterizedTypeException error){
+            Log.e("BlogPostTask", "Malformed URL: " + error);
+        }
+        catch(IOException error) {
+            Log.e("BlogPostTask", "IO Exception: " + error);
+        }
+        return null;
+    }
+};
